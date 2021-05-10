49014d29208351232b971d33f26caf992da36ecf
MeepusDerpus <ion@live.co.za>
2021-05-10 02:38:55 +0200

    Update q3.pl
    rewrite algorithhm

--- q3.pl
+++ q3.pl
@@ -1,11 +1,14 @@
-exp(Base,Exponent,Result):-
+exp(Base,Exponent,Result):-   
+  Exponent =:= 1,
+  write(Result),nl;   
+
   Base > 0,
   Exponent > 0,
   ExponentDec is Exponent - 1,
-  ResultRecur is Base * ExponentDec,
+  ResultRecur is Base * Exponent,
   exp(Base,ExponentDec,ResultRecur).
+  
+
 exp(Base,Exponent,Result):-
-  Exponent =:= 0,
-  Result = 1;
   Base =:= 0,
   Result = 0.
