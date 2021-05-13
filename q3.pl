50f4798aa3f79e2e679d9798cd9681b1cb1e52f5
MeepusDerpus <ion@live.co.za>
2021-05-13 11:27:48 +0200

    Finished Q3
    Finished Cleanup

--- q3.pl
+++ q3.pl
@@ -1,14 +1,21 @@
-exp(Base,Exponent,Result):-   
-  Exponent =:= 1,
-  write(Result),nl;   
-
-  Base > 0,
+exp(_,0,1).
+exp(0,_,0).
+exp(Base,Exponent,Result):-
+  Base =\= 0,
   Exponent > 0,
-  ExponentDec is Exponent - 1,
-  ResultRecur is Base * Exponent,
-  exp(Base,ExponentDec,ResultRecur).
+  Exp1 is Exponent - 1,
+  exp(Base,Exp1,R1),
+  Result is R1*Base.
+
+
+
+
+  
   
 
-exp(Base,Exponent,Result):-
-  Base =:= 0,
-  Result = 0.
+
+
+
+
+
+
